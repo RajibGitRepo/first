13fdfd3e58f664d206e08a6034c23bdde513c7ff
Riju Debnath <debnathriju907@gmail.com>
2023-02-10 12:42:20 +0530

    kdffjd

---
+++ appendchild/index.js
@@ -0,0 +1,35 @@
+// console.log("hello World");
+
+
+// let getUl = document.querySelector("ul");
+
+// let getButton = document.querySelector("button");
+
+// let index = 0;
+
+// let array = ["Rajib","Poku","Riju","Amit","Abhijit","Bisal","Sharmila","indrani"];
+
+// getButton.addEventListener("click", (events)=>{
+
+
+//    let length =  getUl.lastChild.length;
+   
+//         for(let i = 0; i<length;i++){
+//     let createElement = document.createElement("li");
+
+//     createElement.textContent = array[i];
+//     getUl.appendChild(createElement);
+// }
+
+// console.log(now)
+
+
+
+
+
+
+
+
+
+
+
---
+++ secondFoundation/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let driverLisence = false;
+const testPass = true;
+
+if(testPass) driverLisence=true;
+if(driverLisence) console.log("Hello World");
+
+// const interface = "Audio";
+// console.log(interface)
+
+console.log("rajib");
